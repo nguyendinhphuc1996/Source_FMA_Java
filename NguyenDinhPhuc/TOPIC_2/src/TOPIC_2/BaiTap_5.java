3f32b98a32d6a318b609d4c25bbc2654125cd1aa
nguyendinhphuc1996 <nguyendinhphuc1996@gmail.com>
2017-04-30 14:11:28 +0700

    update

--- NguyenDinhPhuc/TOPIC_2/src/TOPIC_2/BaiTap_5.java
+++ NguyenDinhPhuc/TOPIC_2/src/TOPIC_2/BaiTap_5.java
@@ -1,6 +1,8 @@
 package TOPIC_2;
 
+import java.awt.List;
 import java.util.Scanner;
+import java.util.stream.IntStream;
 
 /*
  * 5. Mảng (Array): Viết chương trình nhập vào một mảng số nguyên có n phần tử:
@@ -35,9 +37,18 @@ public class BaiTap_5
 		// ----- Xuất mảng
 		System.out.print("Mảng vừa nhập : ");
 		xuatMang(arrInt);
-		
+
 		// Max, Min
-		System.out.println("\n\nMax = "+MaxValue(arrInt) +" & Min = "+MinValue(arrInt));
+		System.out.println("\n\nMax = " + MaxValue(arrInt) + " & Min = " + MinValue(arrInt));
+		
+		// Tìm, đếm và tính tổng các phần tử là số nguyên tố.
+		int[] arrSNT = filter_PhanTu_SNT(arrInt);
+		
+		System.out.println("\nCác phần tử là Số nguyên tố: ");
+		xuatPhanTu_SNT(arrSNT);
+		
+		System.out.println("Số phần tử là Số nguyên tố: "+arrSNT.length);
+		System.out.println("Tổng phần tử là Số nguyên tố: "+IntStream.of(arrSNT).sum());
 	}
 
 	// ======= FUNCTION =======
@@ -92,5 +103,43 @@ public class BaiTap_5
 				min = i;
 		}
 		return min;
+	}
+
+	static void ResizeArray_Int(int[] $array, int $size)
+	{
+		int newsize = $array.length; // newsize để kiểm tra giới hạn mảng để đưa
+										// phần từ cũ vào
+		if ($size < $array.length)
+			newsize = $size;
+
+		int[] $arrayNew = $array;
+		$array = new int[$size]; // thay đổi kích thước
+		for (int i = 0; i < newsize; i++)
+		{
+			$array[i] = $arrayNew[i];
+		}
+	}
+
+	static int[] filter_PhanTu_SNT(int[] arr)
+	{
+		int[] arrSNT = new int[0];
+		for (int so : arr)
+		{
+			BaiTap_3 obj_BT3 = new BaiTap_3();
+			if (obj_BT3.laSoNguyenTo(so))
+			{
+				ResizeArray_Int(arrSNT, arrSNT.length + 1);
+				arrSNT[arrSNT.length - 1] = so;
+			}
+		}
+		return arrSNT;
+	}
+
+	static void xuatPhanTu_SNT(int[] arr)
+	{
+		for(int i:arr)
+		{
+			System.out.print(i + " ");
+		}
 	}
 }
